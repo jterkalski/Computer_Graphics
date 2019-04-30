f76bf8d3d55a017447ffdc64f39a7f0367ac4a49
Jakub.Terkalski <Jakub.Terkalski@gmail.com>
2019-04-30 08:27:53 +0200

    Add Lab7

---
+++ Lab7/lab7/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace lab7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.filterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.filtersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.redToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.greenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.blueToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inverseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.grayscaleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contrastToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.brightnessToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.filterToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem1});
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.openToolStripMenuItem.Text = "File";
+            // 
+            // filterToolStripMenuItem
+            // 
+            this.filterToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.filtersToolStripMenuItem});
+            this.filterToolStripMenuItem.Name = "filterToolStripMenuItem";
+            this.filterToolStripMenuItem.Size = new System.Drawing.Size(56, 24);
+            this.filterToolStripMenuItem.Text = "Tools";
+            // 
+            // openToolStripMenuItem1
+            // 
+            this.openToolStripMenuItem1.Name = "openToolStripMenuItem1";
+            this.openToolStripMenuItem1.Size = new System.Drawing.Size(216, 26);
+            this.openToolStripMenuItem1.Text = "Open";
+            this.openToolStripMenuItem1.Click += new System.EventHandler(this.openToolStripMenuItem1_Click);
+            // 
+            // filtersToolStripMenuItem
+            // 
+            this.filtersToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.redToolStripMenuItem,
+            this.greenToolStripMenuItem,
+            this.blueToolStripMenuItem,
+            this.inverseToolStripMenuItem,
+            this.grayscaleToolStripMenuItem,
+            this.contrastToolStripMenuItem,
+            this.brightnessToolStripMenuItem});
+            this.filtersToolStripMenuItem.Name = "filtersToolStripMenuItem";
+            this.filtersToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.filtersToolStripMenuItem.Text = "Filters";
+            // 
+            // redToolStripMenuItem
+            // 
+            this.redToolStripMenuItem.Name = "redToolStripMenuItem";
+            this.redToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.redToolStripMenuItem.Text = "Red";
+            this.redToolStripMenuItem.Click += new System.EventHandler(this.redToolStripMenuItem_Click);
+            // 
+            // greenToolStripMenuItem
+            // 
+            this.greenToolStripMenuItem.Name = "greenToolStripMenuItem";
+            this.greenToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.greenToolStripMenuItem.Text = "Green";
+            this.greenToolStripMenuItem.Click += new System.EventHandler(this.greenToolStripMenuItem_Click);
+            // 
+            // blueToolStripMenuItem
+            // 
+            this.blueToolStripMenuItem.Name = "blueToolStripMenuItem";
+            this.blueToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.blueToolStripMenuItem.Text = "Blue";
+            this.blueToolStripMenuItem.Click += new System.EventHandler(this.blueToolStripMenuItem_Click);
+            // 
+            // inverseToolStripMenuItem
+            // 
+            this.inverseToolStripMenuItem.Name = "inverseToolStripMenuItem";
+            this.inverseToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.inverseToolStripMenuItem.Text = "Inverse";
+            this.inverseToolStripMenuItem.Click += new System.EventHandler(this.inverseToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(574, 358);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // grayscaleToolStripMenuItem
+            // 
+            this.grayscaleToolStripMenuItem.Name = "grayscaleToolStripMenuItem";
+            this.grayscaleToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.grayscaleToolStripMenuItem.Text = "Grayscale";
+            this.grayscaleToolStripMenuItem.Click += new System.EventHandler(this.grayscaleToolStripMenuItem_Click);
+            // 
+            // contrastToolStripMenuItem
+            // 
+            this.contrastToolStripMenuItem.Name = "contrastToolStripMenuItem";
+            this.contrastToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.contrastToolStripMenuItem.Text = "Contrast";
+            this.contrastToolStripMenuItem.Click += new System.EventHandler(this.contrastToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // brightnessToolStripMenuItem
+            // 
+            this.brightnessToolStripMenuItem.Name = "brightnessToolStripMenuItem";
+            this.brightnessToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.brightnessToolStripMenuItem.Text = "Brightness";
+            this.brightnessToolStripMenuItem.Click += new System.EventHandler(this.brightnessToolStripMenuItem_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(0, 406);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(574, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(580, 336);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(581, 313);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Contrast:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(580, 258);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(581, 235);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Brightness:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem filterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem filtersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem redToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem greenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem blueToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem inverseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem grayscaleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem contrastToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem brightnessToolStripMenuItem;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Lab7/lab7/Form1.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab7 // filtry punktowe 10/10
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap tmpbmp;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+                tmpbmp = new Bitmap(Image.FromFile(openFileDialog1.FileName));
+            }
+        }
+
+        private void redToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = bmp.Width;
+
+
+            for(int i=0; i<bmp.Width; i++)
+            {
+                for (int j=0; j<bmp.Height; j++)
+                {
+                    //Color colour = bmp.GetPixel(i, j);
+                    Color colour = tmpbmp.GetPixel(i, j);
+
+                    int R = colour.R;
+                    int G = colour.G;
+                    int B = colour.B;
+
+                    // R=0;
+                    G = 0;
+                    B = 0;
+
+                    colour = Color.FromArgb(R, G, B);
+
+                    bmp.SetPixel(i, j, colour);
+                }
+                progressBar1.Value = i;
+            }
+
+            pictureBox1.Image = bmp;
+
+        }
+
+        private void greenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = bmp.Width;
+
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    //Color colour = bmp.GetPixel(i, j);
+                    Color colour = tmpbmp.GetPixel(i, j);
+
+                    int R = colour.R;
+                    int G = colour.G;
+                    int B = colour.B;
+
+                    R=0;
+                   // G = 0;
+                    B = 0;
+
+                    colour = Color.FromArgb(R, G, B);
+
+                    bmp.SetPixel(i, j, colour);
+                }
+                progressBar1.Value = i;
+            }
+
+            pictureBox1.Image = bmp;
+        }
+
+        private void blueToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = bmp.Width;
+
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    //Color colour = bmp.GetPixel(i, j);
+                    Color colour = tmpbmp.GetPixel(i, j);
+
+                    int R = colour.R;
+                    int G = colour.G;
+                    int B = colour.B;
+
+                    R=0;
+                    G = 0;
+                    //B = 0;
+
+                    colour = Color.FromArgb(R, G, B);
+
+                    bmp.SetPixel(i, j, colour);
+                }
+                progressBar1.Value = i;
+            }
+
+            pictureBox1.Image = bmp;
+        }
+
+        private void inverseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = bmp.Width;
+
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    //Color colour = bmp.GetPixel(i, j);
+                    Color colour = tmpbmp.GetPixel(i, j);
+
+                    int R = colour.R;
+                    int G = colour.G;
+                    int B = colour.B;
+
+                    R = 255 - R;
+                    G = 255 - G;
+                    B = 255 - B;
+
+                    colour = Color.FromArgb(R, G, B);
+
+                    bmp.SetPixel(i, j, colour);
+                }
+                progressBar1.Value = i;
+            }
+
+            pictureBox1.Image = bmp;
+        }
+
+        private void grayscaleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = bmp.Width;
+
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    //Color colour = bmp.GetPixel(i, j);
+                    Color colour = tmpbmp.GetPixel(i, j);
+
+                    int R = colour.R;
+                    int G = colour.G;
+                    int B = colour.B;
+
+                    double gray = 0.299 * R + 0.587 * G + 0.114 * B;
+
+                    R = G = B = (int)gray;
+
+
+                    colour = Color.FromArgb(R, G, B);
+
+                    bmp.SetPixel(i, j, colour);
+                }
+                progressBar1.Value = i;
+            }
+
+            pictureBox1.Image = bmp;
+
+        }
+
+        private void contrastToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = bmp.Width;
+
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    //Color colour = bmp.GetPixel(i, j);
+                    Color colour = tmpbmp.GetPixel(i, j);
+
+                    int R = colour.R;
+                    int G = colour.G;
+                    int B = colour.B;
+
+                    int contrast;
+                    int.TryParse(textBox1.Text, out contrast);
+
+                    R = R - 128;
+                    G = G - 128;
+                    B = B - 128;
+
+                    R = R * contrast;
+                    G = G * contrast;
+                    B = B * contrast;
+
+                    R = R + 128;
+                    G = G + 128;
+                    B = B + 128;
+                    
+                    if (R<0) { R = 0; }
+                    else if (R>255) { R = 255; }
+
+                    if (G < 0) { G = 0; }
+                    else if (G > 255) { G = 255; }
+
+                    if (B<0) { B = 0; }
+                    else if (B>255) { B = 255; }
+
+
+
+
+                    colour = Color.FromArgb(R, G, B);
+
+                    bmp.SetPixel(i, j, colour);
+                }
+                progressBar1.Value = i;
+            }
+
+            pictureBox1.Image = bmp;
+
+
+        }
+
+        private void brightnessToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = bmp.Width;
+
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    //Color colour = bmp.GetPixel(i, j);
+                    Color colour = tmpbmp.GetPixel(i, j);
+
+                    int R = colour.R;
+                    int G = colour.G;
+                    int B = colour.B;
+
+
+
+
+
+                    //stopień zmiany koloru
+                    int change; //100
+                    int.TryParse(textBox2.Text, out change);
+
+                    R = R + change;
+                    G = G + change;
+                    B = B + change;
+
+
+                    if (R > 255) { R = 255; }
+                    if (G > 255) { G = 255; }
+                    if (B > 255) { B = 255; }
+
+
+
+
+                    colour = Color.FromArgb(R, G, B);
+
+                    bmp.SetPixel(i, j, colour);
+                }
+                progressBar1.Value = i;
+            }
+
+            pictureBox1.Image = bmp;
+        }
+
+
+
+
+
+
+    }
+}
+
+//filtry morfologiczne - otoczenie piksela wpływa na jego wartość
